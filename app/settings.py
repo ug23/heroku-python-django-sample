49d88c0389b1d457937c8adadec8885b16ab301f
Yuji Imagawa <imagawa@bengo4.com>
2020-11-21 23:35:44 +0900

    secretが入っていたので隠す

--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&omz2(ra5=k%u%q-@_koe4@sz$(zle4i*y$fy=-@jt7r=%g%0x'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
