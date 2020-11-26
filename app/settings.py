9c217312aad7eaf8812d0a12eba656b23bd4a553
Yuji Imagawa <imagawa@bengo4.com>
2020-11-27 00:10:29 +0900

    シークレットキーを隠す

--- app/settings.py
+++ app/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# NOTE: config varsで登録する
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# NOTE: ここにあるドメインの配下でのみ起動する
 ALLOWED_HOSTS = ['*']
 
 
@@ -77,6 +78,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
+        # NOTE: HerokuのConfig Varを読み込む
         default=os.environ['DATABASE_URL']
     )
 }
